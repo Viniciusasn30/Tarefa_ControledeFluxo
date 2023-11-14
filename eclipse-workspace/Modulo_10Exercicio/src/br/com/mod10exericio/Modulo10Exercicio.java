1d37697ca6aa60f9409d6da1fc61f8c6b47a0306
Viniciusasn30 <Viniciusasnascimento@gmail.com>
2023-11-14 13:07:11 -0300

    Mod10

--- eclipse-workspace/Modulo_10Exercicio/src/br/com/mod10exericio/Modulo10Exercicio.java
+++ eclipse-workspace/Modulo_10Exercicio/src/br/com/mod10exericio/Modulo10Exercicio.java
@@ -1,5 +1,36 @@
 package br.com.mod10exericio;
 
 public class Modulo10Exercicio {
+	
+	    public static void main(String[] args) {
 
-}
+	        operacaoMedia();
+
+	    }
+
+	        public static void operacaoMedia() {
+	            Double nota1 = 9.5;
+	            Double nota2 = 9.1;
+	            Double nota3 = 9.5;
+	            Double nota4 = 9.5;
+	            Double nota5 = nota1 + nota2 + nota3 + nota4;
+	            Double media1 = nota5 / 4;
+
+
+	                if (nota5>=7) {
+	                    System.out.println("Aprovado");
+
+	                }else if (nota5 >=5){
+	                System.out.println("Recuperação");
+
+	                }else{ System.out.println("Reprovado");
+
+	                }
+
+
+	        }
+
+	}
+
+
+
